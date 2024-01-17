3120d63e522cff84dbf65fcd3c1076c2fba543b3
kritigupta23 <kriti.gupta@infinite.com>
2024-01-17 15:38:32 +0530

    bank affliation employer ui

--- src/components/pageSpacific/LRORegistrationForm/EmployeeIDForm/EmployeeIDForm.js
+++ src/components/pageSpacific/LRORegistrationForm/EmployeeIDForm/EmployeeIDForm.js
@@ -1,71 +1,76 @@
 import React from "react";
 import Form from "react-bootstrap/Form";
-import "bootstrap/dist/css/bootstrap.min.css";
-import Row from "react-bootstrap/Row";
-import Container from "react-bootstrap/Container";
-import Col from "react-bootstrap/Col";
+import saveIcon from "../../../../assets/images/svgIcons/save-icon.svg"
+import { Button, Col, Row } from "react-bootstrap";
+import arrowRight from "../../../../assets/images/svgIcons/arrow-right.svg"
 
-export default function EmployeeIDForm() {
+export default function EmployeeIDForm(
+  {
+    newTabData,
+    activeTab,
+    setActiveTab,
+  }
+) {
+  const handleNext = () => {
+    const currentTabIndex = newTabData.findIndex(
+      (tab) => tab.id === activeTab.id
+    );
+    if (currentTabIndex < newTabData.length - 1) {
+      setActiveTab(newTabData[currentTabIndex + 1]);
+    }
+  };
   return (
-    <div>
-      <Container>
-        <Row className="justify-content-md-center">
-          <Col>
-            <h4>Employee Identification Information</h4>
-            <br />
-            <Form>
+    <div className="employee-id-form">
+         <div className="address-step-title d-flex justify-content-between mb-3">
+             <h6>Step 3. Employer Identification Information</h6>
+             <div className="address-step-save ">
+                <img src={saveIcon} alt="" /> 
+             </div>
+          </div>
+          <div className="lro-name mb-3">
+             <label>Local Recipient Organization Name:</label> <b>Name</b>
+          </div>
+        <Form>
               <Form.Group className="mb-3" controlId="name">
-                <Form.Label>LRO Name</Form.Label>
+                <Form.Label>Employer Identification Number (EIN)</Form.Label>
                 <Form.Control
                   type="text"
                   className="InputFeild"
                   id="name"
-                  placeholder="300"
+                  placeholder="Enter user ID"
                   readOnly
                 />
               </Form.Group>
-              <Row>
-                <Col>
-                  <div className="fromInput">
-                    <Form.Label>Ein Extension</Form.Label>
-                    <Form.Control type="text" placeholder="" />
-                  </div>
-                </Col>
-                <Col>
-                  <div className="fromInput">
-                    <Form.Label>Ein</Form.Label>
-                    <Form.Control
-                      type="tel"
-                      placeholder="23-7363262"
-                      name="lroEni"
-                      // value={lroEni}
-                      // style={{ borderColor: borderColor.lroEni }}
-                      // onChange={handleInputChange}
-                      // onFocus={handleInputChangeFocus}
-                      // onBlur={handleInputChangeBlur}
-                    />
-                  </div>
-                </Col>
-                <Col>
-                  <div className="fromInput">
-                    <Form.Label>Sub Ordinate Ein</Form.Label>
-                    <Form.Control
-                      type="text"
-                      placeholder="23-7363262"
-                      name="lroSubOrdinateEin"
-                      // value={lroSubOrdinateEin}
-                      // style={{ borderColor: borderColor.lroSubOrdinateEin }}
-                      // onChange={handleInputChange}
-                      // onFocus={handleInputChangeFocus}
-                      // onBlur={handleInputChangeBlur}
-                    />
-                  </div>
-                </Col>
-              </Row>
+              <Form.Group className="mb-3" controlId="name">
+                <Form.Label>Subordinate (EIN)</Form.Label>
+                <Form.Control
+                  type="text"
+                  className="InputFeild"
+                  id="name"
+                  placeholder="Enter Subordinate"
+                  readOnly
+                />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="name">
+                <Form.Label>EIN Extention</Form.Label>
+                <Form.Control
+                  type="text"
+                  className="InputFeild"
+                  id="name"
+                  placeholder="Enter EIN Extension"
+                  readOnly
+                />
+              </Form.Group>   
+              <div className="border-top mt-4"></div>
+              <Row className="mt-4">
+                  <Col>
+                    <Button className="btn-padding" variant="secondary">CLEAR</Button>
+                    </Col>
+                    <Col className="d-flex justify-content-end">
+                    <Button onClick={handleNext} className="btn-padding btn-icon" variant="primary">NEXT <img src={arrowRight} alt="" /></Button>
+                    </Col>
+                  </Row>
             </Form>
-          </Col>
-        </Row>
-      </Container>
     </div>
   );
 }
